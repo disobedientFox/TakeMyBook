ede452a72f6c385dc8ffb4ac7c2ff68ef58fb618
Denis Krutko <artstyleflax@gmail.com>
2019-05-30 00:12:26 +0200

    tests project with several basic tests for EF

---
+++ TakeMyBookTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TakeMyBookTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TakeMyBookTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3206eca2-0c8a-4077-ba5d-9ccb6f4ee9ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TakeMyBookTest/TradesTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TakeMyBook;
+using TakeMyBook.Services;
+
+namespace TakeMyBookTest
+{
+    [TestClass]
+    public class TradesTest
+    {
+        [TestMethod]
+        public void GettingDepartmentsTest()
+        {
+            TradesService Trades = new TradesService();
+
+            var result = Trades.GetDepartments();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void AddABook()
+        {
+            TradesService Trades = new TradesService();
+
+            var result = Trades.AddBook(new Book
+            {
+                author = "Test name",
+                inStock = true,
+                pagesCount = 0,
+                publishingHouse = "Test house",
+                publishYear = 0,
+                title = "Test title"
+            });
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void FailGettingUser()
+        {
+            TradesService Trades = new TradesService();
+
+            var result = Trades.GetReader(null);
+
+            Assert.IsNull(result);
+        }
+    }
+}
